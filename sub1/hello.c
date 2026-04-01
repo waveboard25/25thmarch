d7e174b670a58ddd8ab403ab8f8007c1ba92cd81
waveboard25 <yug3987@gmail.com>
2026-04-01 12:13:33 +0530

    gen aI used

--- sub1/hello.c
+++ sub1/hello.c
@@ -1,10 +1,13 @@
-include <stdio.h>
+#include <stdio.h>
+int add(int a, int b) {
+    return a + b;
+}
 int main()
 {
-	printf("hello world!!")
-	printf("this is the new modification")
-	printf("this is the second new modification")
+	printf("hello world!!");
+	printf("this is the new modification");
+	printf("this is the second new modification");
 
-	printtf("this da rialll third modifiggg")
-	printf("i;m the third person to be here")
+	printf("this da rialll third modifiggg");
+	printf("i;m the third person to be here");
 }
